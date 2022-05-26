e320e3c58fb7fb55c2800dd213a59200164ed944
juan carlos tlaxcala <jtlaxcala.developer@gmail.com>
2022-05-26 23:22:42 +0000

    IBJ-24 FE agregar angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import {MatGridListModule} from '@angular/material/grid-list';
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NoopAnimationsModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent]
 })
